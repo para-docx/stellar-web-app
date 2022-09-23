9edfcaa18860fb28e42a3607ef6fff34c21a2ce0
para-docx <frozendecember2001@gmail.com>
2022-09-23 12:15:18 +0530

    minor changes

--- pages/SearchNasa.js
+++ pages/SearchNasa.js
@@ -5,18 +5,19 @@ import Link from "next/link";
 
 export default function Searchnasa() {
     return (
+        <div className={styles.background}>
             <div className={styles.container}>
                     <div className={styles.main}>
                         <div> <h1>Search the database </h1>  <br />
-                            <QueryImage /> 
-                    </div>      
-                       
-            <footer> 
+                            <QueryImage />
+                        </div>
+            </div>
+            </div>
+            <footer className={styles.footer}> 
             <Link href="/#" >
             <a className="btn btn-dark btn-lg " role="button" aria-pressed="true"> Back Home </a>
             </Link>
             </footer>
-            </div>
         </div>
     );
 }
